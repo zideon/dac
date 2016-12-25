e92cd8d310779ef68dfa7ba3f609d139f41a9965
zideon <zideon@zideon-270E5J-2570EJ>
2016-12-25 21:42:37 -0200

    Classes de modelo

---
+++ dac/dac-web/src/main/java/br/uff/ic/modelo/RegistroSala.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.ic.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
+
+/**
+ *
+ * @author zideon
+ */
+@Entity
+@SequenceGenerator(name = "SequenciaRegistroSala", sequenceName = "seq_id_registro_sala")
+public class RegistroSala implements Serializable {
+    @Id
+    @GeneratedValue(generator = "SequenciaRegistroSala", strategy = GenerationType.AUTO)
+    private Long ID;
+    
+    @OneToOne
+    private TipoRegistro tipo;
+    
+    @OneToOne
+    private ReservaSala reserva;
+}
--- dac/dac-web/src/main/java/br/uff/ic/modelo/Usuario.java
+++ dac/dac-web/src/main/java/br/uff/ic/modelo/Usuario.java
@@ -10,6 +10,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 
 /**
@@ -17,24 +18,21 @@ import javax.persistence.SequenceGenerator;
  * @author fabio
  */
 @Entity
-@SequenceGenerator(name = "SequenciaUsuario", sequenceName = "seq_id_usuario")
 public class Usuario implements Serializable {
 
-     public Long getID() {
-        return ID;
-    }
-    
     @Id
-    @GeneratedValue(generator = "SequenciaUsuario", strategy = GenerationType.AUTO)
-    private Long ID;
+    private String cpf;
     
     private String login;
     private String senha;
+    private String email;
     
-    public void setID(Long ID) {
-        this.ID = ID;
-    }
+    private String endereço;
+    private String telefone;
 
+    @ManyToOne
+    private Papel papel;
+    
     public String getLogin() {
         return login;
     }
@@ -47,8 +45,48 @@ public class Usuario implements Serializable {
         return senha;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+
+    public Papel getPapel() {
+        return papel;
+    }
+
+    public void setPapel(Papel papel) {
+        this.papel = papel;
     }
     
 }
